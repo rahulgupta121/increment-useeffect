ba1b907ca214b902daeb7092db06a1ddd699db44
rahulgupta121 <rahulkumar3.1996@gmail.com>
2022-01-18 17:37:37 +0530

    commit for update

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -2,8 +2,31 @@ import React from 'react'
 
 const DropDownMenu = () => {
     const ClickMe =()=>{
-        alert("i am clicked")
+        var click = document.getElementById("myDropdown");  
+         if (click.style.display === 'none'){
+             click.style.display = 'grid';
+         } else{
+            click.style.display = 'none';
+         }
     }
+
+    // const ClickMe2 =()=>{
+    //     var click = document.getElementById("myDropdown2");  
+    //      if (click.style.display === 'none'){
+    //          click.style.display = 'grid';
+    //      } else{
+    //         click.style.display = 'none';
+    //      }
+    // }
+
+    // const ClickMe3 =()=>{
+    //     var click = document.getElementById("myDropdown3");  
+    //      if (click.style.display === 'none'){
+    //          click.style.display = 'grid';
+    //      } else{
+    //         click.style.display = 'none';
+    //      }
+    // }
     return (
         <>
         <div className='backGround '>
@@ -14,6 +37,33 @@ const DropDownMenu = () => {
             </div>   
         </div>
          
+        <ul id="myDropdown" class="dropdown-content">
+            <li className='myDropdown-li'> <a href="#option1">Option 1</a></li>
+            <li className='myDropdown-li'><a href="#option2">Option 2</a> </li>
+            <li className='myDropdown-li'><a href="#option3">Option 3</a> </li>
+        </ul>
+        {/*
+        <div className='btn-clickMe-div'>
+                <button onClick={ClickMe2} className='btn-clickMe'>CLICK ME</button>
+            </div>   
+        
+         
+        <div id="myDropdown3" class="dropdown-content">
+            <a href="#option1">Option 1</a>
+            <a href="#option2">Option 2</a>
+            <a href="#option3">Option 3</a>
+        </div>
+
+        <div className='btn-clickMe-div'>
+                <button onClick={ClickMe3} className='btn-clickMe'>CLICK ME</button>
+            </div>   
+        
+         
+        <div id="myDropdown" class="dropdown-content">
+            <a href="#option1">Option 1</a>
+            <a href="#option2">Option 2</a>
+            <a href="#option3">Option 3</a>
+        </div> */}
         </div>
         </>
     )
